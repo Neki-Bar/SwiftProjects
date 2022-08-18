a5815a15e21a500828795631d2b0d8c2ae7b588e
Neki-Bar <71463785+Neki-Bar@users.noreply.github.com>
2022-08-19 01:28:42 +0300

    SomeFeathuresOfWrappedProp

---
+++ Tamagotchi/3.0TamagotchiDontDoThisWithProp.swift
@@ -0,0 +1,154 @@
+import Foundation
+
+protocol Tamagotchi: AnyObject {
+    var foodReserve: Int { get set }
+    var waterReserve: Int { get set }
+ 
+    func eat()
+    func drink()
+    func sleep()
+}
+
+@propertyWrapper
+struct Reserve {
+    private let maxValue: Int
+    private var value: Int = 0
+    var wrappedValue: Int {
+        set { 
+            if newValue < 0 { underZeroHandler() }
+            value =  newValue > maxValue ? maxValue : newValue 
+        } 
+        get { return value }
+    }
+
+    var underZeroHandler: () -> Void = {} // underZeroHandler
+
+    init(maxValue: Int) {
+        self.maxValue = maxValue
+    }
+}
+
+final class SimpleTamagotchi: Tamagotchi {
+    //Знатно поигрался с пропертями. Инкапсулировал логику пропертей чтоб для каждой реализации Tamagotchi не
+    //писать простыню из приватных и вычисляемых пропертей и их геттеров и сеттеров
+    @Reserve(maxValue: 5) var foodReserve: Int
+    @Reserve(maxValue: 5) var waterReserve: Int
+    @Reserve(maxValue: 5) var sleepReserve: Int
+
+    private var zeroFoodHandler = {
+        print("Your tamagotchi is dead!")
+    }
+
+    
+    func eat() {
+        foodReserve += 1
+    }
+
+    func drink() {
+        waterReserve += 1
+    }
+
+    func sleep() {
+        sleepReserve += 3
+    }
+
+    func play() {
+        foodReserve -= 1
+        waterReserve -= 2
+        sleepReserve -= 2
+    }
+
+    func addFoodHandler() {
+        _foodReserve.underZeroHandler = zeroFoodHandler
+    }
+
+    func printPropeprties() {
+        print("Food: \(foodReserve), water: \(waterReserve), sleep: \(sleepReserve)")
+    }
+}
+
+final class WorkingTamagotchi{
+    @Reserve(maxValue: 5) var foodReserve: Int
+    @Reserve(maxValue: 5) var waterReserve: Int
+    @Reserve(maxValue: 5) var sleepReserve: Int
+
+    private var zeroFoodHandler = {
+        print("Your tamagotchi is dead!")
+    }
+
+    func eat() {
+        foodReserve += 2 //этот тамагочи знатно кушает и работает
+    }
+
+    func work() {
+        foodReserve -= 2
+        waterReserve -= 2
+        sleepReserve -= 3
+    }
+
+    func addFoodHandler() {
+        _foodReserve.underZeroHandler = zeroFoodHandler
+    }
+
+    func printPropeprties() {
+        print("Food: \(foodReserve), water: \(waterReserve), sleep: \(sleepReserve)")
+    }
+}
+
+extension WorkingTamagotchi: Tamagotchi {
+    func drink() {
+        waterReserve += 2
+    }
+    func sleep() {
+        sleepReserve += 3
+    }
+}
+
+func testSimpleTamagotchi() {
+    print("SimpleTamagotchi test")
+    let boni: SimpleTamagotchi = SimpleTamagotchi()
+    boni.printPropeprties()
+    boni.addFoodHandler()
+    boni.eat()
+    boni.drink()
+    boni.sleep()
+    boni.printPropeprties()
+    boni.play()
+    boni.printPropeprties()
+    boni.play() // очень интересно. Каждый раз когда проводится операция с едой тамагочи пишет что он мертв.
+    boni.play()
+}
+
+func testWorkingTamagotchi() {
+    print("WorkingTamagotchi test")
+    let boni: WorkingTamagotchi = WorkingTamagotchi()
+    boni.printPropeprties()
+    boni.addFoodHandler()
+    boni.eat()
+    boni.drink()
+    boni.sleep()
+    boni.printPropeprties()
+    boni.work()
+    boni.printPropeprties()
+    boni.work() // очень интересно. Каждый раз когда проводится операция с едой тамагочи пишет что он мертв.
+    boni.work()
+}
+
+testSimpleTamagotchi()
+testWorkingTamagotchi()
+
+//output
+/*
+SimpleTamagotchi test
+Food: 0, water: 0, sleep: 0
+Food: 1, water: 1, sleep: 3
+Food: 0, water: -1, sleep: 1
+Your tamagotchi is dead!
+Your tamagotchi is dead!
+WorkingTamagotchi test
+Food: 0, water: 0, sleep: 0
+Food: 2, water: 2, sleep: 3
+Food: 0, water: 0, sleep: 0
+Your tamagotchi is dead!
+Your tamagotchi is dead!
+*/
